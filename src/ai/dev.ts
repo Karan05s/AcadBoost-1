244ceef79240734de4dbf831fa431716eba6d56f
Karan05s <karanajinathshirsat123@gmail.com>
2025-12-30 10:18:35 +0530

    Add files via upload

---
+++ src/ai/dev.ts
@@ -0,0 +1,7 @@
+import { config } from 'dotenv';
+config();
+
+import '@/ai/flows/generate-study-plan.ts';
+import '@/ai/flows/recommend-personalized-content.ts';
+import '@/ai/flows/summarize-course-material.ts';
+import '@/ai/flows/educhat-flow.ts';
---
+++ src/lib/types.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    profilePhotoUrl?: string;
+    address?: string;
+    phoneNumber?: string;
+    timeSpent?: number;
+}
